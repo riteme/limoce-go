9111eefab828310a095075d0dcb73ca69d7bb1ab
riteme <1412803389@qq.com>
2016-03-16 22:01:59 +0800

    Be able to place chesses

--- src/frontend.py
+++ src/frontend.py
@@ -9,7 +9,11 @@ current_block.size = (BLOCK_RADIUS * 2, BLOCK_RADIUS * 2)
 current_block.position = (INVALID_X, INVALID_Y)
 current_block.fill_color = CURRENT_SELECT_COLOR
 
+current = board.BLACK
+
 def do_events(window):
+    global current
+
     for event in window.events:
         if type(event) is CloseEvent:
             window.close()
@@ -22,6 +26,15 @@ def do_events(window):
         elif type(event) is MouseMoveEvent:
             i, j = board.to_index(event.position.x, event.position.y)
             current_block.position = board.to_position(i, j, BLOCK_RADIUS)
+
+        elif type(event) is MouseButtonEvent:
+            if event.pressed:
+                continue
+
+            i, j = board.to_index(event.position.x, event.position.y)
+            board.set_chess(i, j, current)
+
+            current = board.reverse(current)
 
 if __name__ == "__main__":
     window = RenderWindow(VideoMode(WINDOW_WIDTH, WINDOW_HEIGHT), WINDOW_TITLE,  WINDOW_STYLE)
